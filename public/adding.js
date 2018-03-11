221ed19f6b0d4b429b50a48ad5e9677d1ccd417e
Akbishnu <Akbishnukumar@gmail.com>
2018-03-11 14:41:28 +0530

    added

---
+++ public/adding.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$.tableAdding=function(array){
+		$("#t1").append('<tr><th>'+array[0]+'</th><th>'+array[1]+'</th><th>'+array[2]+'</th><th>'+array[3]+'</th><th>'+array[4]+'</th></tr>');
+        }
+});
